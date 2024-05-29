da3f33f2c4ee5a646ecfb98313e05cea77e40144
Pathmaveni <pathmaveni33@gmail.com>
2024-05-29 13:59:34 +0530

    Add files via upload

---
+++ JS project/Formvalidation.js
@@ -0,0 +1,28 @@
+let username=document.getElementById("username")
+let email=document.getElementById("email")
+let password=document.getElementById("password")
+let cpassword=document.getElementById("cpassword")
+let form=document.getElementById("form")
+form.addEventListener("submit",(e)=>{
+    if(!validation()){
+        e.preventDefault()
+    }
+})
+function validation(){
+    let username_value=username.value   
+    let email_value=email.value
+    let password_value=password.value
+    let cpassword_value=cpassword.value
+    let success=true
+
+    if(username_value==""){
+        success=false
+        set_error(username,"Please fill this box")
+    }
+}
+function set_error(tag,message){
+    let parentTag=tag.parentElement
+    console.log(parentTag)
+    let err=parentTag.getElementById("error")
+    err.innerText=message
+}
